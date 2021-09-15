97771364630ff14267e352a766bffca4e169ed2b
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2021-09-14 20:33:28 -0400

    create GetMapping outline

--- src/main/java/com/example/demo/image/ImageController.java
+++ src/main/java/com/example/demo/image/ImageController.java
@@ -1,14 +1,16 @@
 package com.example.demo.image;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/image")
 public class ImageController {
     @GetMapping
-    public List<Image> hello() {
+    public List<Image> getImages() {
 
         return List.of(
                 new Image(
